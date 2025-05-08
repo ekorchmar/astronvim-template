f1670acf733426cb604f071e1c89a56a114ac2a9
Eduard Korchmar <korchmar.eduard@gmail.com>
2025-05-08 23:41:53 +0200

    feat: import plugin settings from old config

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,54 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
+local AC = "astrocommunity."
+local P = AC .. "pack."
+local E = AC .. "editing-support."
+
 ---@type LazySpec
-return {
+local spec = {
+
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = AC .. "docker.lazydocker" },
+  { import = AC .. "recipes.vscode" },
+
+  { import = P .. "rainbow-delimiter-indent-blankline" },
+
+  { import = E .. "dial-nvim" },
+  { import = E .. "hypersonic-nvim" },
+  { import = E .. "nvim-origami" },
+  { import = E .. "nvim-regexplainer" },
+  { import = E .. "nvim-treesitter-context" },
+  { import = E .. "stickybuf-nvim" },
+  { import = E .. "quick-scope" },
+  { import = E .. "suda-vim" },
+  { import = E .. "todo-comments-nvim" },
+  { import = E .. "undotree" },
 }
+
+-- Languages
+local langs = {
+  "bash",
+  "cpp",
+  "docker",
+  "html-css",
+  "json",
+  "just",
+  "lua",
+  "markdown",
+  "ps1",
+  "python-ruff",
+  "rust",
+  "sql",
+  "toml",
+  "typst",
+  "yaml",
+}
+
+for _, lang in pairs(langs) do
+  ---@diagnostic disable-next-line
+  table.insert(spec, { import = P .. lang })
+end
+
+return spec
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,22 +1,7 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
--- Here are some examples:
+local COPILOT_ENABLED = os.getenv "DISABLE_COPILOT" == nil
 
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
-
-  -- == Examples of Overriding Plugins ==
 
   -- customize dashboard options
   {
@@ -25,64 +10,203 @@ return {
       dashboard = {
         preset = {
           header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
+            [[┌──────────────────────────────────────────────┐]],
+            [[│                                              │]],
+            [[│         _               _     _              │]],
+            [[│        (_)             | |   | |             │]],
+            [[│  __   ___ _ __ ___     | |__ | |___      __  │]],
+            [[│  \ \ / / | '_ ` _ \    | '_ \| __\ \ /\ / /  │]],
+            [[│   \ V /| | | | | | |_  | |_) | |_ \ V  V /   │]],
+            [[│    \_/ |_|_| |_| |_( ) |_.__/ \__| \_/\_/    │]],
+            [[│                    |/                        │]],
+            [[│                                              │]],
+            [[└──────────────────────────────────────────────┘]],
           }, "\n"),
         },
       },
     },
   },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      extra_groups = {
+        "NormalFloat",
+        "NvimTreeNormal",
+      },
+    },
+    config = function(_, opts)
+      local transparent = require "transparent"
+      transparent.setup(opts)
+      transparent.clear_prefix "BufferLine"
+      transparent.clear_prefix "NeoTree"
+      transparent.clear_prefix "lualine"
+    end,
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts)
+          opts.mappings.n["<Leader>ux"] = { "<Cmd>TransparentToggle<CR>", desc = "Toggle transparency" }
+          if vim.tbl_get(opts, "autocmds", "heirline_colors") then
+            table.insert(opts.autocmds.heirline_colors, {
+              event = "User",
+              pattern = "TransparentClear",
+              desc = "Refresh heirline colors",
+              callback = function()
+                if package.loaded["heirline"] then require("astroui.status.heirline").refresh_colors() end
+              end,
+            })
+          end
+        end,
+      },
+    },
+  },
+  -- GitHub Copilot (if allowed)
+  {
+    "github/copilot.vim",
+    cond = COPILOT_ENABLED,
+    build = ":Copilot auth",
+    config = function()
+      vim.g.copilot_filetypes = { markdown = true }
+      vim.g.copilot_no_tab_map = true
+      -- Accept suggestions with Ctrl+a
+      vim.keymap.set("i", "<C-a>", 'copilot#Accept("")', {
+        expr = true,
+        replace_keycodes = false,
+      })
     end,
   },
-
   {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+    "CopilotC-Nvim/CopilotChat.nvim",
+    cond = COPILOT_ENABLED,
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts) opts.mappings.n["<Leader>@"] = { "<Cmd>CopilotChat<CR>", desc = "Copilot chat" } end,
+      },
+    },
+    opts = {},
+  },
+
+  -- Oil
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>-"] = { function() require("oil").open() end, desc = "Open folder in Oil" },
+            },
+          },
+          autocmds = {
+            oil_settings = {
+              {
+                event = "FileType",
+                desc = "Disable view saving for oil buffers",
+                pattern = "oil",
+                callback = function(args) vim.b[args.buf].view_activated = false end,
+              },
+              {
+                event = "User",
+                pattern = "OilActionsPost",
+                desc = "Close buffers when files are deleted in Oil",
+                callback = function(args)
+                  if args.data.err then return end
+                  for _, action in ipairs(args.data.actions) do
+                    if action.type == "delete" then
+                      local _, path = require("oil.util").parse_url(action.url)
+                      local bufnr = vim.fn.bufnr(path)
+                      if bufnr ~= -1 then require("astrocore.buffer").wipe(bufnr, true) end
+                    end
+                  end
+                end,
+              },
+            },
+          },
         },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
+      },
+      {
+        "rebelot/heirline.nvim",
+        optional = true,
+        dependencies = {
+          "AstroNvim/astroui",
+          opts = { status = { winbar = { enabled = { filetype = { "^oil$" } } } } },
+        },
+        opts = function(_, opts)
+          if opts.winbar then
+            local status = require "astroui.status"
+            table.insert(opts.winbar, 1, {
+              condition = function(self) return status.condition.buffer_matches({ filetype = "^oil$" }, self.bufnr) end,
+              status.component.separated_path {
+                padding = { left = 2 },
+                max_depth = false,
+                suffix = false,
+                path_func = function(self) return require("oil").get_current_dir(self.bufnr) end,
+              },
+            })
+          end
+        end,
+      },
+    },
+    opts = function()
+      local get_icon = require("astroui").get_icon
+      return {
+        view_options = {
+          show_hidden = true,
+        },
+        win_options = {
+          signcolumn = "yes:2",
+        },
+        constrain_cursor = "name",
+        columns = {
+          "permissions",
+          "size",
+          "mtime",
+          { "icon", default_file = get_icon "DefaultFile", directory = get_icon "FolderClosed" },
+        },
+      }
     end,
+  },
+  {
+    "refractalize/oil-git-status.nvim",
+    ft = "oil",
+    opts = {
+      show_ignored = true, -- show files that match gitignore with !!
+      symbols = { -- customize the symbols that appear in the git status columns
+        index = {
+          ["!"] = "",
+          ["?"] = "",
+          ["A"] = "",
+          ["C"] = "",
+          ["D"] = "",
+          ["M"] = "",
+          ["R"] = "",
+          ["T"] = "",
+          ["U"] = "",
+        },
+        working_tree = {
+          ["!"] = "",
+          ["?"] = "",
+          ["A"] = "",
+          ["C"] = "",
+          ["D"] = "",
+          ["M"] = "",
+          ["R"] = "",
+          ["T"] = "",
+          ["U"] = "",
+        },
+      },
+    },
+  },
+
+  -- Dynamic line number coloring
+  {
+    "mawkler/modicator.nvim",
+    opts = {},
   },
 }
