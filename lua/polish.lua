83c4c520901a74c4213094e91cf3c53e148e34f1
Eduard Korchmar <korchmar.eduard@gmail.com>
2025-05-08 23:42:01 +0200

    feat: small adjustments

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,27 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+vim.g.autoformat = false
+vim.opt.cmdheight = 2
+vim.opt.colorcolumn = "81"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+-- Listchars
+local listchars = {
+    tab = ">-",
+    -- eol = '↲',
+    -- space = '⋅', -- A lot of clutter;
+    trail = "•",
+    extends = "◀",
+    precedes = "▶",
+}
+
+vim.opt.list = true
+vim.opt.listchars = listchars
+vim.opt.showbreak = "↪"
+
+-- Remove "o" from fo-table - disables automated comments
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "r", "o" })
+    end,
+})
